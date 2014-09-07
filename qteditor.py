8498ef1e41750fa1d68fe942413bcca4e6e75a9c
Dharmit Shah <dharmit.dev@gmail.com>
2014-09-07 09:48:31 +0530

    Cut-Copy-Paste and Undo-Redo - Part 1

--- qteditor.py
+++ qteditor.py
@@ -45,6 +45,57 @@ class Main(QtGui.QMainWindow):
         self.previewAction.setShortcut("Ctrl+Shift+P")
         self.previewAction.triggered.connect(self.preview)
 
+        # Cut action
+        self.cutAction = QtGui.QAction(QtGui.QIcon("icons/cut.png"), "Cut",
+                                       self)
+        self.cutAction.setStatusTip("Cut to clipboard")
+        self.cutAction.setShortcut("Ctrl+X")
+        self.cutAction.triggered.connect(self.text.cut)
+
+        # Copy action
+        self.copyAction = QtGui.QAction(QtGui.QIcon("icons/copy.png"), "Copy",
+                                        self)
+        self.copyAction.setStatusTip("Copy to clipboard")
+        self.copyAction.setShortcut("Ctrl+C")
+        self.copyAction.triggered.connect(self.text.copy)
+
+        # Paste action
+        self.pasteAction = QtGui.QAction(QtGui.QIcon("icons/paste.png"),
+                                         "Paste", self)
+        self.pasteAction.setStatusTip("Paste from clipboard")
+        self.pasteAction.setShortcut("Ctrl+V")
+        self.pasteAction.triggered.connect(self.text.paste)
+
+        # Undo action
+        self.undoAction = QtGui.QAction(QtGui.QIcon("icons/undo.png"), "Undo",
+                                        self)
+        self.undoAction.setStatusTip("Undo last action")
+        self.undoAction.setShortcut("Ctrl+Z")
+        self.undoAction.triggered.connect(self.text.undo)
+
+        # Redo action
+        self.redoAction = QtGui.QAction(QtGui.QIcon("icons/redo.png"), "Redo",
+                                        self)
+        self.redoAction.setStatusTip("Redo last undone thing")
+        self.redoAction.setShortcut("Ctrl+Y")
+        self.redoAction.triggered.connect(self.text.redo)
+
+        # Bullets
+        self.bulletAction = QtGui.QAction(QtGui.QIcon("icons/bullet.png"),
+                                          "Insert bullet list", self)
+        self.bulletAction.setStatusTip("Insert bullet list")
+        self.bulletAction.setShortcut("Ctrl+Shift+B")
+        self.bulletAction.triggered.connect(self.bulletList)
+
+        # Numbered list
+        self.numberedAction = QtGui.QAction(QtGui.QIcon("icons/number.png"),
+                                            "Insert numbered list", self)
+        self.numberedAction.setStatusTip("Insert numbered list")
+        self.numberedAction.setShortcut("Ctrl+Shift+L")
+        self.numberedAction.triggered.connect(self.numberList)
+
+        # Now instantiate a toolbar of the class addToolBar
+
         self.toolBar = self.addToolBar("Options")
 
         self.toolBar.addAction(self.newAction)
@@ -54,6 +105,19 @@ class Main(QtGui.QMainWindow):
         self.toolBar.addSeparator()
         self.toolBar.addAction(self.printAction)
         self.toolBar.addAction(self.previewAction)
+
+        self.toolBar.addSeparator()
+        self.toolBar.addAction(self.cutAction)
+        self.toolBar.addAction(self.copyAction)
+        self.toolBar.addAction(self.pasteAction)
+
+        self.toolBar.addSeparator()
+        self.toolBar.addAction(self.undoAction)
+        self.toolBar.addAction(self.redoAction)
+
+        self.toolBar.addSeparator()
+        self.toolBar.addAction(self.bulletAction)
+        self.toolBar.addAction(self.numberedAction)
 
         self.toolBar.addSeparator()
 
@@ -67,12 +131,22 @@ class Main(QtGui.QMainWindow):
         file = menuBar.addMenu("File")
         edit = menuBar.addMenu("Edit")
         view = menuBar.addMenu("View")
+        insert = menuBar.addMenu("Insert")
 
         file.addAction(self.newAction)
         file.addAction(self.openAction)
         file.addAction(self.saveAction)
         file.addAction(self.printAction)
         file.addAction(self.previewAction)
+
+        edit.addAction(self.undoAction)
+        edit.addAction(self.redoAction)
+        edit.addAction(self.cutAction)
+        edit.addAction(self.copyAction)
+        edit.addAction(self.pasteAction)
+
+        insert.addAction(self.bulletAction)
+        insert.addAction(self.numberedAction)
 
     def initUI(self):
         self.text = QtGui.QTextEdit(self)
@@ -130,6 +204,13 @@ class Main(QtGui.QMainWindow):
         if dialog.exec_() == QtGui.QDialog.Accepted:
             self.text.document().print_(dialog.printer())
 
+    def bulletList(self):
+        cursor = self.text.textCursor()
+        cursor.insertList(QtGui.QTextListFormat.ListDisc)
+
+    def numberList(self):
+        cursor = self.text.textCursor()
+        cursor.insertList(QtGui.QTextListFormat.ListDecimal)
 
 def main():
     app = QtGui.QApplication(sys.argv)
